d0db87bc2e92adc4e20e732b55ff851fd8022b7a
오원준 <kbw0309@douzone.com>
2023-10-21 13:52:56 +0900

    구조 변경

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,17 +1,38 @@
-import React, {useEffect, useRef} from "react";
-
+import React, {useState,useEffect, useRef} from "react";
+import SkillSet from "../SkillSet/SkillSet"
 const Portfolio = () => {
+    const [idx,setIdx] = useState(1)
+
+    const onClickHeader=(e)=>{
+        switch(e.target.id){
+            case "tab1":
+                setIdx(1)
+                break;
+            case "tab2":
+                setIdx(2)
+                break;
+            case "tab3":
+                setIdx(3)
+                break;
+           case "tab4":
+                setIdx(4)
+                break;
+           case "tab5":
+                setIdx(5)
+                break;
+        }
+    }
 
     return (
         <div>
             <header className="Portfolio-Header">
                 <h1>Portfolio.</h1>
                 <nav className="navbar">
-                    <a href="#" className="active" style={{'--i': 1}}>Home</a>
-                    <a href="#" style={{'--i': 2}}>About</a>
-                    <a href="#" style={{'--i': 3}}>Portfolio</a>
-                    <a href="#" style={{'--i': 4}}>Skills</a>
-                    <a href="#" style={{'--i': 5}}>Contact</a>
+                    <button id="tab1" onClick={onClickHeader} className={idx ===1?"active": ""} style={{'--i': 1}}>Home</button>
+                    <button id="tab2" onClick={onClickHeader} className={idx ===2?"active": ""} style={{'--i': 2}}>About</button>
+                    <button id="tab3" onClick={onClickHeader} className={idx ===3?"active": ""} style={{'--i': 3}}>Portfolio</button>
+                    <button id="tab4" onClick={onClickHeader} className={idx ===4?"active": ""} style={{'--i': 4}}>Skills</button>
+                    <button id="tab5" onClick={onClickHeader} className={idx ===5?"active": ""} style={{'--i': 5}}>Contact</button>
                 </nav>
             </header>
 
@@ -32,6 +53,7 @@ const Portfolio = () => {
                     <img src="/assets/Image.png" alt="Profile"/>
                 </div>
             </section>
+            {/*<SkillSet/>*/}
         </div>
     );
 };
